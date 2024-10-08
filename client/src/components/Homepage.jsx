ed59336da294518b3b7a977f78c54944c301e021
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-08 13:17:41 +0000

    feat: added contents for the homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,22 +1,28 @@
 import React from 'react'; 
 
 
-function Home() {
-    
-    
-    return (
-    
-    <div className="w-screen h-screen bg-neutral-800">
-        
-     <div className='flex justify-center items-center'>       
-        <h1 className='color-white'>Create your own shoes.</h1>
-        <h1 className='color-white'>make your ideas into reality.</h1>
-    
-     </div>
 
-    
-    </div>
+function Home() {
+    return (
+        
+        
+        <div className="w-screen h-screen bg-neutral-800 columns-2">
+            
+            
+            <div className='flex flex-col h-full px-10 py-28 '>
+                <h1 className='text-white text-6xl font-bold mb-4'>Create your own shoes,</h1>
+                <h1 className='text-white text-4xl'>Make your ideas into reality.</h1>
+            </div>
+
+            <div className='flex flex-col justify-center items-center h-full'>
+
+            </div>
+        
+        
+        
+        
+        </div>
     );
 }
 
-export default Home
+export default Home;
