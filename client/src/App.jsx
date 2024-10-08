7f57bd9e610562dded7beae319d8d9934747cfa0
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-08 11:31:16 +0000

    add: home page contents

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <>     
+      
       <BrowserRouter>
         <NavBar />
         <Home />
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -5,13 +5,17 @@ function Home() {
     
     
     return (
-    <>
     
     <div className="w-screen h-screen bg-neutral-800">
         
+     <div className='flex justify-center items-center'>       
+        <h1 className='color-white'>Create your own shoes.</h1>
+        <h1 className='color-white'>make your ideas into reality.</h1>
+    
+     </div>
+
+    
     </div>
-        
-    </>
     );
 }
 
