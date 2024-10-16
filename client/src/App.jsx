9c36324ff0c2655a58af3167df876d18de354232
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-16 12:27:00 +0000

    feat: added routes for shop and customize page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 import Home from './components/Homepage';
 import Chatbot  from './components/Chatbot'
 import NavBar from './components/NavBar'
+import Customize from './components/Customize'
 
 function App() {
 
@@ -15,7 +16,14 @@ function App() {
         <NavBar />
         <Home />
         <Chatbot />
+
+      
+        <Routes>
+        <Route path="/customize" element ={<Customize />}/>
+        </Routes>
+
       </BrowserRouter>
+
 
     </>
   )
