7920149c6017bc94d44a5d9a5e87fec061bcc985
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-17 11:10:51 +0000

    fix: issue when clicking chatbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
       
       <BrowserRouter>
         <NavBar />
-        <Chatbot />
 
       
         <Routes>
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import React from 'react';
 import { FaExternalLinkAlt } from "react-icons/fa";
 import {motion} from 'framer-motion';
+import {Link} from 'react-router-dom'
 
+import Chatbot from "./Chatbot";
+import Customize from "./Customize";
 
 function Home() {
 
@@ -36,7 +39,10 @@ function Home() {
                 <h1 className='text-white text-6xl font-bold mb-3'>Create your own shoes,</h1>
                 <h1 className='text-neutral-100 text-4xl mb-5'>Let us make your ideas into reality.</h1>
                 <p className='text-neutral-300 text-md mb-5'>with shoepify you can make your ideas into reality <br/> by costumizing your own shoes.</p>
+                <Link to='/customize'>
                 <button className='flex justify-center items-center text-white w-40 rounded-full bg-transparent border px-3 py-3 hover:bg-neutral-600'>Customize now <FaExternalLinkAlt className='ml-2' /> </button>
+                </Link>
+
                 
             </div>
 
@@ -44,7 +50,7 @@ function Home() {
                 <motion.img
                     key = {home_images[currentIndex]}
                     src = {home_images[currentIndex]}
-                    alt= 'Error 404 Not Found'
+                    alt= 'Error 404 Not Found'  
                     className="h-full w-3/4 "
 
                     initial={{opacity: 0, scale: 0.9, x: 50}}
@@ -56,7 +62,7 @@ function Home() {
             </div>
         
         
-        
+        <Chatbot/>
         
         </div>
     );
