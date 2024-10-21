8d84a8dfcf445c325677e9f2b53bc80c3c27c910
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-21 12:16:35 +0000

    feat: dropdown menu. fix gui

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 function NavBar() {
+
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    }
+
 
     return(
     <>
@@ -10,14 +17,56 @@ function NavBar() {
     <div className='fixed flex justify-end items-center space-x-2 bg-neutral-900 cursor-pointer w-screen h-16 text-md text-neutral-300 pr-20 z-50'>
 
         <Link to="/shop" ><div className='hover:bg-neutral-700 h-14 px-3 content-center'>Shop</div></Link>   
-
-        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className=" hover:bg-neutral-700 px-3 text-center h-14 inline-flex items-center" type="button">About
-
-        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-        <       path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        <button
+        id="dropdownDefaultButton"
+        onClick={toggleDropdown}
+        className="hover:bg-neutral-700 px-3 text-center h-14 inline-flex items-center"
+        type="button"
+      >
+        About
+        <svg
+          className="w-2.5 h-2.5 ms-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
         </svg>
+      </button>
 
-        </button>
+      {/* Dropdown Menu */}
+            {isDropdownOpen && (
+                <div
+                id="dropdown"
+                className="fixed top-16 right-1/4 bg-white divide-y divide-gray-100  shadow w-44 dark:bg-neutral-700 "
+                >
+                <ul className="py-2 text-sm text-neutral-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                    <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-neutral-600 dark:hover:text-white">
+                        About Us
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-neutral-600 dark:hover:text-white">
+                        Services
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-neutral-600 dark:hover:text-white">
+                        Contact
+                    </a>
+                    </li>
+                    
+                </ul>
+                </div>
+            )}
 
         <div className='hover:bg-neutral-700 h-14 px-3 content-center'>Services</div>
         <div className='hover:bg-neutral-700 h-14 px-3 content-center'>Contact Us</div>
