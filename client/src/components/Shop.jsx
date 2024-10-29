0cc31764c2e5bf71d2af80bee9413eb0f5e95c50
Kit Magtulis <102131789+keetmagtulis@users.noreply.github.com>
2024-10-29 12:16:50 +0000

    feat: fixed shop UI

--- client/src/components/Shop.jsx
+++ client/src/components/Shop.jsx
@@ -6,9 +6,25 @@ function Shop() {
 return(
  <>
     
-<div className="flex items-center justify-center h-screen">
-        
-<h1 className="text-5xl text-black">Shop</h1>
+<div className="flex flex-col items-center justify-center">
+
+    <div>
+        <h2 className='font-bold text-3xl mt-20 mb-10'>SHOP</h2>
+    </div>
+
+        <div className='grid grid-cols-4 grid-rows-2 gap-x-5 gap-y-5'>
+
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+            <div className="w-80 h-80 bg-neutral-200 rounded-lg"></div>
+
+        </div>
 
 
 
